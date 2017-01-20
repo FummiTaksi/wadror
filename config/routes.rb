45f0725e52c7c97e5cf4875550fff38bec790808
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-01-20 18:11:28 +0200

    Tehty viikko2 t4asti

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  root 'beers#index'
+  root 'breweries#index'
+  get 'ratings', to: 'ratings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
