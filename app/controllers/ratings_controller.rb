4d1cb237c6296d16a77d034020e09533753da08b
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-02-07 10:37:24 +0200

    korjattu ongelma herokussa.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,14 +10,16 @@ class RatingsController < ApplicationController
 
   def create
     @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-    if @rating.save
+
+    if current_user.nil?
+      redirect_to signin_path, notice: "You should be signed in"
+    elsif @rating.save
       current_user.ratings << @rating
-      redirect_to current_user
+      redirect_to user_path current_user
     else
       @beers = Beer.all
       render :new
     end
-
   end
 
   def destroy
